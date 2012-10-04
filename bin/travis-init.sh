ff298553cc0ccec3fa155419ed7a24d9c83a36d3
Salimane Adjao Moustapha <me@salimane.com>
2012-10-05 01:46:19 +0800

    some travis env debug

--- bin/travis-init.sh
+++ bin/travis-init.sh
@@ -1,11 +1,12 @@
-redis-server ./bin/redis/redis-node-1.conf
-redis-server ./bin/redis/redis-node-2.conf
-redis-server ./bin/redis/redis-node-5.conf
-redis-server ./bin/redis/redis-node-6.conf
+sh -c "redis-server /home/travis/builds/salimane/rediscluster-php/bin/redis/redis-node-1.conf"
+sh -c "redis-server /home/travis/builds/salimane/rediscluster-php/bin/redis/redis-node-2.conf"
+sh -c "redis-server /home/travis/builds/salimane/rediscluster-php/bin/redis/redis-node-5.conf"
+sh -c "redis-server /home/travis/builds/salimane/rediscluster-php/bin/redis/redis-node-6.conf"
 wget -O phpredis.tar.gz --no-check-certificate https://github.com/nicolasff/phpredis/tarball/master
 tar -xzf phpredis.tar.gz
 sh -c "cd nicolasff-phpredis-* && phpize && ./configure && make && sudo make install"
 echo "extension=redis.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 phpenv rehash
+ps aux | grep redis-server
 wget http://getcomposer.org/composer.phar
 php composer.phar install
